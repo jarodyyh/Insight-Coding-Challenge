cd9516d161973226ce264d6dbb862d3a3b11f6dc
jarodyyh <jarodyyh@gmail.com>
2017-04-05 02:51:09 -0500

    Insight Code Challenge

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+# I'll execute my programs, with the input directory log_input and output the files in the directory log_output
+python  ./src/process_log.py  ./log_input/log.txt  ./log_output/hosts.txt  ./log_output/hours.txt  ./log_output/resources.txt  ./log_output/blocked.txt
+
